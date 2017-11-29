a95216392e7137ed193adff64eada4d6c900174c
Mpintor <miguel.pintor.moral@accenture.com>
2017-11-29 15:38:06 +0100

    Added more unit test to the preferred language query.

--- test/unit/queries.test.js
+++ test/unit/queries.test.js
@@ -6,13 +6,30 @@ const QueriesDefinitions = require('../../src/core/queries');
 describe('queries.js', () => {
   it('if the user has no repository then the preferred language is undefined', () => {
     let output = {data : { user: { repositories: { nodes : []}}}};
-    assert.ok(!QueriesDefinitions.preferredLanguage.parseOutput(output));
+    assert.ok(QueriesDefinitions.preferredLanguage.parseOutput(output) === undefined);
   });
 });
 
 describe('queries.js', () => {
   it('if the user has repositories then a preferred language is expected', () => {
-    let output = {data : { user: { repositories: { nodes : [{ primaryLanguage : {name: 'JavaScript'}},{ primaryLanguage : {name: 'JavaScript'}}]}}}};
+    let output = {data : { user: { repositories: { nodes : [{ primaryLanguage : {name: 'JavaScript'}}, { primaryLanguage : {name: 'Java'}}, { primaryLanguage : {name: 'JavaScript'}}]}}}};
     assert.equal(QueriesDefinitions.preferredLanguage.parseOutput(output), 'JavaScript');
+  });
+});
+
+describe('queries.js', () => {
+  it('if the user has repositories without a preferred language then that repository should be ignored', () => {
+    let output = {data : { user: { repositories: { nodes : [{ primaryLanguage : {name: 'JavaScript'}}, { primaryLanguage : null}, { primaryLanguage : {name: 'Java'}}, { primaryLanguage : null}, { primaryLanguage : {name: 'JavaScript'}}]}}}};
+    assert.equal(QueriesDefinitions.preferredLanguage.parseOutput(output), 'JavaScript');
+  });
+});
+
+describe('queries.js', () => {
+  it('if the user has the same preference for several languages then the last ocurrence is the valid one', () => {
+    let output = {data : { user: { repositories: { nodes : [
+      { primaryLanguage : {name: 'JavaScript'}}, { primaryLanguage : {name: 'Java'}}, 
+      { primaryLanguage : {name: 'JavaScript'}}, { primaryLanguage : {name: 'Java'}},
+      { primaryLanguage : {name: 'Ruby'}}, { primaryLanguage : {name: 'Ruby'}}]}}}};
+    assert.equal(QueriesDefinitions.preferredLanguage.parseOutput(output), 'Ruby');
   });
 });
